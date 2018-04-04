d086597eed6b2f2f80b53747870e9a15be044557
kanishkarj <kanishkarj@hotmail.com>
2018-04-04 16:01:38 +0530

    Added request data fields for student registration

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -10,7 +10,7 @@ const auth = passport.authenticate('jwt', {session: false});
 
 const cors = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 }
 
